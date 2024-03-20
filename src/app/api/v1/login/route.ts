946115fa6324bc0c3676e3adc89705219a3d7129
devdeer-kevin <kevin.heyland@devdeer.com>
2024-03-20 21:39:41 +0100

    Add login route POST handler doc

--- src/app/api/v1/login/route.ts
+++ src/app/api/v1/login/route.ts
@@ -1,5 +1,11 @@
 import { NextResponse, NextRequest } from 'next/server'
 
+/**
+ * Handles the POST request for the login route.
+ *
+ * @param req - The NextRequest object representing the incoming request.
+ * @returns A NextResponse object with the appropriate status and message.
+ */
 export async function POST(req: NextRequest) {
     const body = await req.json()
     const password = process.env.SCOTTY_PASSWORD as string
