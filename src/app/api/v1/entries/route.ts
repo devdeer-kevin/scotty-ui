038343b6d7d3b378a34467c769c274054fd9f1f1
devdeer-kevin <kevin.heyland@devdeer.com>
2024-03-20 21:37:59 +0100

    Refactor API route.ts file

--- src/app/api/v1/entries/route.ts
+++ src/app/api/v1/entries/route.ts
@@ -2,14 +2,15 @@ import { NextResponse, NextRequest } from 'next/server'
 
 export async function POST(req: NextRequest) {
     const body = await req.json()
-    const password = process.env.USER_SCOTTY_API_PASSWORD as string
-    const user = process.env.USER_SCOTTY_API_USER as string
     const from = body.isoStartDate as string
     const to = body.isoEndDate as string
     const clientName = body.clientName as string
     const taskTypesToIgnore = ['Managed Service', 'Fasttrack'] as string[]
 
-    const url = 'https://api-dd-scotty-prod.azurewebsites.net/api/v2/CoffeeCup/timeEntries/Export'
+    const password = process.env.USER_SCOTTY_API_PASSWORD as string
+    const user = process.env.USER_SCOTTY_API_USER as string
+    const url = process.env.SCOTTY_API as string
+
     const response = await fetch(url, {
         method: 'POST',
         headers: {
