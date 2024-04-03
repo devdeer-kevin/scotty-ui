5da3681d1a86428bb43c56846503c14f598db4d0
devdeer-kevin <kevin.heyland@devdeer.com>
2024-04-03 20:57:49 +0200

    Refactor home page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,15 @@ import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center">
-            <div className="max-w-4xl items-center justify-center font-mono text-sm flex-col gap-4">
-                <div className="pb-4">
-                    <h1 className="text-4xl font-bold text-left text-slate-700">Welcome to Scotty</h1>
-                </div>
-                <div className="flex justify-center">
-                    <LoginLink className="bg-slate-200 p-2 rounded-md" postLoginRedirectURL="/file-export">
-                        Sign in
-                    </LoginLink>
-                </div>
+        <main className="bg-gray-900 p-8 rounded-xl shadow-xl">
+            <div className="pb-4">
+                <h3 className="text-xl font-normal text-center text-slate-700">Welcome to</h3>
+                <h1 className="text-4xl font-bold text-center text-slate-700">Scotty UI</h1>
+            </div>
+            <div className="flex justify-center">
+                <LoginLink className="bg-slate-200 p-2 rounded-md" postLoginRedirectURL="/file-export">
+                    Sign in
+                </LoginLink>
             </div>
         </main>
     )
