47c7b87d7836060d86eea9a874e3dc155ea2ec41
devdeer-kevin <kevin.heyland@devdeer.com>
2024-04-15 21:37:38 +0200

    Add Zustand package for state management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components'
 export default function Home() {
     return (
         <main className="bg-gray-900 p-8 rounded-xl shadow-xl">
-            <div className="pb-4">
-                <h3 className="text-xl font-normal text-center text-slate-700">Welcome to</h3>
-                <h1 className="text-4xl font-bold text-center text-slate-700">Scotty UI</h1>
-            </div>
+            <nav>
+                <div className="pb-4">
+                    <h3 className="text-xl font-normal text-center text-slate-700">Welcome to</h3>
+                    <h1 className="text-4xl font-bold text-center text-slate-700">Scotty UI</h1>
+                </div>
+            </nav>
             <div className="flex justify-center">
                 <LoginLink className="bg-slate-200 p-2 rounded-md" postLoginRedirectURL="/file-export">
                     Sign in
