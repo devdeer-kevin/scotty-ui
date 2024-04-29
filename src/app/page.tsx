026a10b7e2be8735a51f21d27baacf519136518f
devdeer-kevin <kevin.heyland@devdeer.com>
2024-04-29 20:44:49 +0200

    Add redirection if logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
+
 export default async function Home() {
-    return (
+    const { isAuthenticated } = getKindeServerSession()
+
+    return (await isAuthenticated()) ? (
+        <>{redirect('/file-export')}</>
+    ) : (
         <>
             <div className="text-slate-50">Please sign in to continue.</div>
         </>
